9f120bf7234474f196a193546e22e83bac8a3496
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2020-12-03 02:36:49 -0500

    restream body parse issue

--- src/api/index.js
+++ src/api/index.js
@@ -9,6 +9,18 @@ const git_hash = require('./git_hash');
 
 const router = express.Router();
 
+// retream parsed body before proxying
+const restream = (proxyReq, req, res, options) => {
+  if (req.body) {
+    let bodyData = JSON.stringify(req.body);
+    // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
+    proxyReq.setHeader('Content-Type','application/json');
+    proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+    // stream the content
+    proxyReq.write(bodyData);
+  }
+}
+
 router.get('/', (req, res) => {
   res.json({
     message: 'API - 👋🌎🌍🌏'
@@ -19,21 +31,17 @@ router.use('/gitHash', git_hash);
 
 router.use('/:hash', createProxyMiddleware({
   target: `${process.env.FORWARD_URL}`,
-  // target: `http://localhost:3000`,
   router: async (req) => {
     const object = await utils.readFile(GIT_HASH_DATA_PATH);
 
     const temp = object[req.params.hash];
 
     return `${process.env.FORWARD_URL}:${temp.port}`;
-    // return `http://localhost:3000`;
   },
   changeOrigin: true,
-  // pathRewrite: {
-  //   [`^/json_placeholder`]: '',
-  // },
   pathRewrite: (path, req) => path.replace('/api/v1/' + req.params.hash, ''),
   logLevel: 'debug',
+  onProxyReq: restream
 }));
 
 module.exports = router;
